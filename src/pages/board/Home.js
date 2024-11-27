9bc9b3cd38d57e8f0889802e6e801fd664b0e2ea
hmya_hnya <kh120205@gmail.com>
2024-11-27 16:22:20 +0900

    241127pm

--- src/pages/board/Home.js
+++ src/pages/board/Home.js
@@ -1,30 +1,55 @@
 import { useEffect, useState } from "react";
 import AxiosApi from "../../api/AxiosApi";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
   const [members, setMembers] = useState([]);
 
   useEffect(() => {
     const fetchMemberList = async () => {
-      const response = await AxiosApi.memberList();
-      setMembers(response.data); // 서버에서 받은 데이터를 상태에 저장
+      try {
+        const response = await AxiosApi.memberList();
+        console.log(response.data);
+        setMembers(response.data); // 서버에서 받은 데이터를 상태에 저장
+      } catch (e) {
+        alert("서버가 응답하지 않습니다." + e);
+      }
     };
     fetchMemberList();
   }, []);
+
   return (
-    <div>
+    <>
       <h1>Home</h1>
-      <p>여기가 Home입니다.</p>
-      {members.length > 0 ? (
+      <h4>회원 리스트</h4>
+      <br />
+      <tr>
+        <th>이름</th>
+        <th>이메일</th>
+        <th>가입일</th>
+      </tr>
+      {members &&
+        members.map((member) => (
+          <tr key={member.email}>
+            <td>{member.name}</td>
+            <td>{member.email}</td>
+            <td>{member.date}</td>
+          </tr>
+        ))}
+
+      {/* {members.length > 0 ? (
         <ul>
           {members.map((member) => (
-            <li key={member.id}>{`${member.name} (${member.email})`}</li>
+            <li
+              key={member.id}
+            >{`- 이름: ${member.name}|| 이메일: ${member.email}`}</li>
           ))}
         </ul>
       ) : (
         <p>회원 정보가 없습니다.</p>
-      )}
-    </div>
+      )} */}
+    </>
   );
 };
 
